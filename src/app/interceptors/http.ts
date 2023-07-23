460e50f1ed51b42de6b70875851b062e184ae2d9
1amardeep <amardeepsinghgahlot@gmail.com>
2023-07-23 13:25:58 +0530

    changes for enviroment file support

--- src/app/interceptors/http.ts
+++ src/app/interceptors/http.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { environment } from '../../environments/environment';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: environment.apiUrl,
   headers: {
     // Authorization: 'Bearer your_access_token',
     'Content-Type': 'application/json',
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: '/api',
+  apiKey: 'YOUR_API_KEY',
+};
